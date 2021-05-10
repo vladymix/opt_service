8d462bffeea61157f69319d6cf591f23b77b15ad
josedevicente <jose.c.d.v@gmail.com>
2021-05-10 17:56:28 +0200

    Primera implementación de prueba de GET request en servidor.

--- prototype/server/server.js
+++ prototype/server/server.js
@@ -3,6 +3,7 @@ const http = require('http');
 const hostname = '127.0.0.1';
 const port = 3000;
 
+// web server
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
@@ -12,3 +13,25 @@ const server = http.createServer((req, res) => {
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
+
+// GET request
+const options = {
+  hostname: hostname, // 'whatever.com',
+  port: port,         // 443,
+  path: '/',          // '/todos',
+  method: 'GET'
+}
+
+const req = https.request(options, res => {
+  console.log(`statusCode: ${res.statusCode}`)
+
+  res.on('data', d => {
+    process.stdout.write(d)
+  })
+})
+
+req.on('error', error => {
+  console.error(error)
+})
+
+req.end()
