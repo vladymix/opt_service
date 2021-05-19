f2f749752f936cc5905b1f9a109ff3b7b46bdcb9
Fabricio Altamirano <fabricio.altamirano@exaccta.com>
2021-05-19 10:03:23 +0200

    Creado proyecto android

---
+++ test_app/app/src/main/java/com/ssr/otp/services/MyFirebaseMessaginService.kt
@@ -0,0 +1,11 @@
+package com.ssr.otp.services
+
+import com.google.firebase.messaging.FirebaseMessagingService
+
+class MyFirebaseMessaginService: FirebaseMessagingService() {
+
+    override fun onNewToken(p0: String) {
+        super.onNewToken(p0)
+
+    }
+}
